21ead0d03d104c045c002f8f016feb0c4ceba40b
unknown <toghrul@code.edu.az>
2022-08-18 16:28:15 +0400

    .

---
+++ 00.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork0
+{
+      static class ExtentionMethods
+    {
+        public static bool IsOdd(int num)
+        {
+            if (num % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+       public static  bool IsEven(int num)
+        {
+            if (num % 2 ==0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static  bool IsContainsDigit(string str)
+        {
+            if (str == "digit character")
+            {
+                return true;
+            }
+
+            return false;    
+           
+        }
+        public  static string ToCapitalize(this string str)
+        {
+            StringBuilder a = new StringBuilder();
+            a.Append(Char.ToUpper(str[0]));
+            a.Append(str.Substring(1).ToLower());
+            return a.ToString(); 
+        }
+        public static int []  GetValueIndexes(this string str,char c)
+        {
+            int[] Array = new int[0];
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]==c)
+                {
+                    Array (ref Array, Array.Length + 1);
+                    Array[^1] = i;
+                }
+            }
+            return Array;
+        }
+       
+
+
+    }
+      
+
+}
+
---
+++ 11.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            int[] Arr = "Bakı".GetValueIndexes('a');
+            foreach (int item in Arr)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
